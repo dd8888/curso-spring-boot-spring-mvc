7d79f33962cda1edf6ca92bfda5cfcd19cd850de
dd8888 <doubled000000@gmail.com>
2020-06-22 15:50:32 +0100

    crear demo estructura proyecto

---
+++ src/test/java/com/openwebinars/estructura/EstructuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openwebinars.estructura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstructuraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
