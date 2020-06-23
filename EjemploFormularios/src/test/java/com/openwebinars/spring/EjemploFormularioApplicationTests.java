3621e3bc8f5edb7b95cc9918a48eac8c59d7bd1d
dd8888 <doubled000000@gmail.com>
2020-06-23 13:14:23 +0100

    Ejemplo Formularios

---
+++ EjemploFormularios/src/test/java/com/openwebinars/spring/EjemploFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openwebinars.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploFormularioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
