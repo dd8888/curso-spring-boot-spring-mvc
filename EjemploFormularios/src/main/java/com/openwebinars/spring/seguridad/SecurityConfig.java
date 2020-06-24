2720d51856f99d426ada9cbb7e6d910c637db65b
dd8888 <doubled000000@gmail.com>
2020-06-24 23:03:01 +0100

    Ejemplo seguridad y login

---
+++ EjemploFormularios/src/main/java/com/openwebinars/spring/seguridad/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.openwebinars.spring.seguridad;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+
+/*Implementar y poner la anotacion hace muchas cosas automáticas.
+
+    "Al integrar Spring Security (a través de la dependencia starter)
+    en un proyecto web, y crear la clase que extiende a WebSecurityConfigurerAdapter
+    con la anotación @EnableWebSecurity, se activan muchos elementos de seguridad por defecto.
+    Entre ellos, un mecanismo de login a través de una plantilla por defecto.
+
+*/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }*/
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+
+
+
+    }
+}
