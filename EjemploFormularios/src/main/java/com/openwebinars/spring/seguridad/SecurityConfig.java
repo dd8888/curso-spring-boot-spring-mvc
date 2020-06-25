e5d542fe602066b08e895feedcde88bb258ba4fc
dd8888 <doubled000000@gmail.com>
2020-06-25 22:30:12 +0100

    Ejercicio login seguridad

--- EjemploFormularios/src/main/java/com/openwebinars/spring/seguridad/SecurityConfig.java
+++ EjemploFormularios/src/main/java/com/openwebinars/spring/seguridad/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.openwebinars.spring.seguridad;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,10 +20,19 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /*@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/webjars/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
 
-    }*/
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,8 +41,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password("admin")
                 .roles("ADMIN");
-
-
-
     }
 }
